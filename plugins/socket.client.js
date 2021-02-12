ac30091550b4375a869bcb45b181a62e7679bde3
Luiz Almeida <luiz.garra@live.com>
2021-02-12 11:30:34 -0300

    optsa

--- plugins/socket.client.js
+++ plugins/socket.client.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import VueSocketIO from 'vue-socket.io'
+import SocketIO from 'socket.io-client'
+
+const socketConnection = SocketIO('https://alemdoportaomovement.com/')
 
 export default function () {
   Vue.use(new VueSocketIO({
-    debug: false,
-    connection: 'https://alemdoportaomovement.com/'
-    // options: { path: '/api/' }
-  }))
+    debug: true,
+    connection: socketConnection
+  })
+  )
 }
