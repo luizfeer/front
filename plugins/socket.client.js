419f823cffa3bb7f091882f30188dd7f0b0f0a29
Luiz Almeida <luiz.garra@live.com>
2021-02-12 12:10:01 -0300

    a

--- plugins/socket.client.js
+++ plugins/socket.client.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueSocketIO from 'vue-socket.io'
 import SocketIO from 'socket.io-client'
 
-const socketConnection = SocketIO('https://alemdoportaomovement.com/')
+const socketConnection = SocketIO('https://alemdoportaomovement.com/api/')
 
 export default function () {
   Vue.use(new VueSocketIO({
